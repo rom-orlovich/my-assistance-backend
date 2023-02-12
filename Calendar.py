8073352d040a1251c4b9fd0440618157b5e6cdb4
Rom Orlovich <romorlovich@gmail.com>
2023-02-13 01:53:22 +0200

    create commands manager in Calender

--- Calendar.py
+++ Calendar.py
@@ -3,7 +3,9 @@ from flask import redirect, session
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from datetime import datetime
-from typing import List, Dict
+from typing import List, Dict, TypeVar
+from CalenderChat import CalenderChat
+
 from my_types import Message
 from Event import Event
 import types
@@ -11,112 +13,9 @@ import types
 from enum import Enum
 
 
-class CalenderChatMode(Enum):
-    GET_EVENTS = 1
-    CREATE_EVENTS = 2
-
-
-# start
-# end
-# location
-# description
-# summary
-
-
-class CalenderChat:
-    def __init__(self):
-        self.is_active = False
-        self.mode = None
-        self.create_events_index = 0
-        self.get_events_index = 0
-        self.create_events_opt = {}
-        self.get_events_opt = {}
-        self.create_events_questions_list = [("When the event will start?", "start"),
-                                             ("when the event will end?", "end"), ("Where the event will place?", "locations"), ("May you want add description?", "description")]
-        self.wait_for_answer = False
-
-    def manage_chat(self, content):
-        self.get_create_event_questions(self)
-
-    def check_next_index_valid(self):
-        if self.create_events_index + 1 <= len(self.create_events_questions_list)-1:
-            return False
-
-    def check_pre_index_valid(self):
-        if self.create_events_index - 1 >= 0:
-            return False
-
-    def get_create_events_response(self, content) -> None:
-        if not self.wait_for_answer:
-            return
-
-        if self.mode is not CalenderChatMode.CREATE_EVENTS:
-            return
-        if not content:
-            return
-        self.create_events_opt[self.get_create_event_prop()] = content
-        if self.create_events_index + 1 == len(self.create_events_questions_list):
-            return
-        self.create_events_index += 1
-        self.wait_for_answer = False
-
-    def get_create_event_question(self) -> str:
-        return self.create_events_questions_list[self.create_events_index][0]
-
-    def get_create_event_prop(self) -> str:
-        return self.create_events_questions_list[self.create_events_index][1]
-
-    def get_create_event_questions(self) -> str:
-        if self.wait_for_answer:
-            return
-        if self.mode is not CalenderChatMode.CREATE_EVENTS:
-            return
-
-        if self.create_events_index < 0:
-            return
-        cur_question = self.create_events_questions_list[self.create_events_index][0]
-        self.wait_for_answer = True
-        return cur_question
-
-    def toggle_activation(self, content: str) -> None:
-        if "/calender" in content.lower():
-            self.is_activate = True
-        if "/calender/end" in content.lower():
-            self.reset_calender_chat()
-
-    def change_mode(self, content: str) -> None:
-        if "/events/get" in content.lower():
-            self.mode = CalenderChatMode.GET_EVENTS
-        if "/events/create" in content.lower():
-            self.mode = CalenderChatMode.CREATE_EVENTS
-        else:
-            self.mode = None
-
-    def traverse_question(self, content: str):
-        if 'back' in content.lower():
-            if not self.check_pre_index_valid():
-                return
-            self.create_events_index -= 1
-            self.wait_for_answer = False
-
-        if 'next' in content.lower():
-            if not self.check_next_index_valid():
-                return
-            self.create_events_index -= 1
-            self.wait_for_answer = False
-
-    def reset_calender_chat(self) -> None:
-        self.is_active = False
-        self.mode = None
-        self.create_events_index = 0
-        self.get_events_index = 0
-        self.create_events_opt = {}
-        self.get_events_opt = {}
-
-
 class Calendar:
     def __init__(self) -> None:
-        self.calender_chat = CalenderChat()
+        self.chat_manager = CalenderChat()
 
     def __print_events(self, events: Dict[str, any]):
         response = ""
@@ -168,12 +67,25 @@ class Calendar:
 
         return "The event was create successfully"
 
+    def manage_commands(self, content: str, query: TypeVar("T", dict)):
+        if "/events/get/submit" in content:
+            return self.get_events(query)
+        elif "/events/create/submit" in content:
+            return self.create_events(query)
+
     def parse_message(self, content: str):
         if not content:
             return
         if "\calendar" not in content:
             return
-        self.calender_chat.toggle_activation(content)
+        if self.chat_manager.manage_commands(content):
+            pass
+        conversation = self.chat_manager.conversation()
+        if conversation:
+            return conversation
+        results = self.chat_manager.submit_results()
+        if results:
+            pass
 
         # self.calender_chat["mode"] = True
 
--- Event.py
+++ Event.py
@@ -30,33 +30,33 @@ class Event:
         except ValueError as err:
             return False
 
-    # def __get_normalize_event(self, event_opt: List[str]):
-        normalize_event = {}
-        for event in self.event_opt:
-            event_split = event.split("=")
-            if len(event_split) == 0:
-                continue
-            elif self.__get_event_opt_key(event_split[0]) is None:
-                continue
-            else:
-                prop_values_opt_split = event_split[1].split(",")
-                if len(prop_values_opt_split) == 1:
-                    normalize_event[event_split[0]] = event_split[1]
-                else:
-                    sub_values = {}
-                    for prop_values_opt in prop_values_opt_split:
-                        sub_values_opt_split = prop_values_opt.split("-")
-                        if len(sub_values_opt_split) == 0:
-                            continue
-                        else:
-                            value = self.__create_date(sub_values_opt_split[1])
-                            if value is False:
-                                return
-                            else:
-                                sub_values[sub_values_opt_split[0]
-                                           ] = sub_values_opt_split[1]
-                    normalize_event[event_split[0]] = sub_values
-        return normalize_event
+    # # def __get_normalize_event(self, event_opt: List[str]):
+    #     normalize_event = {}
+    #     for event in self.event_opt:
+    #         event_split = event.split("=")
+    #         if len(event_split) == 0:
+    #             continue
+    #         elif self.__get_event_opt_key(event_split[0]) is None:
+    #             continue
+    #         else:
+    #             prop_values_opt_split = event_split[1].split(",")
+    #             if len(prop_values_opt_split) == 1:
+    #                 normalize_event[event_split[0]] = event_split[1]
+    #             else:
+    #                 sub_values = {}
+    #                 for prop_values_opt in prop_values_opt_split:
+    #                     sub_values_opt_split = prop_values_opt.split("-")
+    #                     if len(sub_values_opt_split) == 0:
+    #                         continue
+    #                     else:
+    #                         value = self.__create_date(sub_values_opt_split[1])
+    #                         if value is False:
+    #                             return
+    #                         else:
+    #                             sub_values[sub_values_opt_split[0]
+    #                                        ] = sub_values_opt_split[1]
+    #                 normalize_event[event_split[0]] = sub_values
+    #     return normalize_event
 
 
 # \calendar\events\create?start=dateTime-07/02/2023 16:00:00?end=dateTime-07/02/2023 20:00:00?summary=Google I/O 2015?location=800 Howard St., San Francisco, CA 94103?description=A chance to hear more about Google\'s developer products

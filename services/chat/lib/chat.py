18a18cf3a20d212eb814f59a8929aaea98bdb980
Rom Orlovich <romorlovich@gmail.com>
2023-02-14 23:57:52 +0200

    finish add comments in command.py

--- services/chat/lib/chat.py
+++ services/chat/lib/chat.py
@@ -5,6 +5,9 @@ import datetime
 
 
 class Chat:
+    """ Chat is class that manage the chat between the bot and the users.
+    """
+
     def __init__(self) -> None:
         self.messages = list()
         self.message_id = 0
@@ -12,18 +15,31 @@ class Chat:
                         "how are you?": 'Fine!', "what are you doing?": "Nothing.", "what is the time?": datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")})
         self.create_message(self.bot.get_bot_response(""))
 
-    def create_message(self, message: Message):
+    def create_message(self, message: Message) -> None:
+        """ 
+        Create a new message dict that will append to the message list.
+        After new message was created the message_id will increase by one.
+        Args:
+            message (Message): Message class 
+        """
         self.message_id += 1
         new_message = {"message_id": self.message_id,
                        "date": datetime.datetime.now().isoformat(), "is_bot": False, **message}
 
         self.messages.append(new_message)
 
-    def manage_chat(self, message: Message):
+    def manage_chat(self, message: Message) -> int:
+        """_summary_
+
+        Args:
+            message (Message): A new message from the user.
+
+        Returns:
+            _type_: message_id
+        """
         self.create_message(message)
         bot_response = self.bot.get_bot_response(message.get("content"))
         self.create_message(bot_response)
-
         return self.message_id
 
     def get_messages(self):
--- services/chat/lib/command.py
+++ services/chat/lib/command.py
@@ -16,6 +16,11 @@ from services.chat.lib.chat_types import ParamOption, ParamsLocations
 
 
 class Command:
+    """
+        Command is class that its core is a Trie words data structure. 
+        If sentence will match the command the callback will execute with the parameters if they exist.
+    """
+
     def __init__(self, cb: Callable, parameters_metadata: Dict[str, ParamOption] = None) -> None:
         """
         Args:
@@ -52,10 +57,12 @@ class Command:
             return token, key
         return None
 
-    def add_command(self, command: str):
-        """ Use Trie algorithm.
+    def add_command(self, command: str) -> None:
+        """ 
+        Use Trie algorithm.
         Words that already exist in the Trie node, will attach to the next node.
         Also, the methods analyze where node the token will be placed.
+
         Args:
             command (str): a sentence that will execute the cb. 
         """
@@ -86,11 +93,20 @@ class Command:
         root_word["end"] = True
 
     # Add list of commands.
-    def add_commands(self, commands: List[str]):
+    def add_commands(self, commands: List[str]) -> None:
         for command in commands:
             self.add_command(command)
 
-    def get_parameters(self, words: List[str], params_indexes: List[ParamsLocations]):
+    def get_parameters(self, words: List[str], params_indexes: List[ParamsLocations]) -> Union[Dict, None]:
+        """
+        Slice the values of the parameters by their start and end position relative to the words list and generate a parameter dict. 
+
+        Args:
+            words (List[str]): A List of the words from the command.
+            params_indexes (List[ParamsLocations]): A indexes list of where the parameter's value is start and end relative to the words list.   
+        Returns:
+            Union[Dict, None]: If the command has parameters_metadata the method will return the a dict from the extracted parameters value. 
+        """
         if not self.parameters_metadata:
             return
         parameters = {}
@@ -100,7 +116,17 @@ class Command:
             parameters[metadata.field_name] = " ".join(param_value)
         return parameters
 
-    def execute(self, command: str):
+    def execute(self, command: str) -> any:
+        """ 
+        Loop over the provided command words and check if the current command is match the the any sequence of words in the Trie data structure.
+        If it does the command will execute the callback. 
+
+        Args:
+            command (str): The command that the method will check if it match to any sequence of words in the Trie data structure.
+
+        Returns:
+            _type_: The result of the function.
+        """
         words = self.split_sentence_to_words(command)
         cur_word = self.words
         params_indexes: List[ParamsLocations] = []
@@ -126,22 +152,21 @@ class Command:
 
         # Check where is the first position of parameter location and save its index and his name.
             if not startIndex and cur_param:
-
                 startIndex = i
                 cur_word = cur_word["$"]
-
                 cur_param_name = cur_param.get("token")
 
+        # Append the last parameter index start index until the end of the words list.
+        # This block only happen when the last word that match the command is parameter.
         if cur_param_name:
             params_indexes.append(
                 ParamsLocations(cur_param_name, startIndex, len(words)))
 
-        # self.words.get(cur_param_name) or self.words.get(word)
         parameters = self.get_parameters(words, params_indexes)
-
         res = None
-        if cur_word.get("end"):
 
+        # If the last word has the indicator end, the sentence match to the command and the callback will execute.
+        if cur_word.get("end"):
             if parameters:
                 res = self.cb(parameters)
             else:
